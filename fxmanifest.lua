c202917ddcf53fafc6e1025e6741900ec5ae3945
MobScripts <113401404+MobScripts@users.noreply.github.com>
2022-09-17 19:52:31 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'V3CT0RBUG'
-description 'Boss utilities by V3CT0RBUG'
+description 'Mob VehicleShop'
 version '1.0'
 
 server_script 'main.lua'
 
 mbvehicle_updater 'yes'
-mbvehicle_name '^2mob_vehicleshop'
+mbvehicle_name '^2Mob VehicleShop'
 mbvehicle_github 'https://github.com/MobScripts/mob_vehicleshop'
 
